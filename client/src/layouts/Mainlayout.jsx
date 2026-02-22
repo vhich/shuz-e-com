c2e78b82fdf0ef33764a63d141947fbe40d3e4a0
victor chi <victoriamglorious@gmail.com>
2026-02-22 13:07:26 +0100

    Fix case sensitivity for ToTopBtn

--- client/src/layouts/Mainlayout.jsx
+++ client/src/layouts/Mainlayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import ToTopBtn from "../components/toTopBtn";
+import ToTopBtn from "../components/ToTopBtn";
 import Loading from "../components/Loading";
 import { ToastContainer } from "react-toastify";
 import BlockageUI from "../components/BlockageUI";

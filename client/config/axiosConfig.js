bfba02a19496ddfd2aa2b62ac0dfa5222a13c0f8
victor chi <victoriamglorious@gmail.com>
2026-02-22 14:28:56 +0100

    solving issue for deployment

--- client/config/axiosConfig.js
+++ client/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shuz-e-com-backend.onrender.com",
+  baseURL: "https://shuz-e-com-backend.onrender.com/api",
   withCredentials: true,
 });
 
